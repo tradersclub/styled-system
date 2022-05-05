05357bb46d8662bd4fbb66bcbebd7109e83265f2
Hebert Silva <hebertpereirasilva@gmail.com>
2022-05-05 11:04:32 -0300

    fix(PW-393): merge to feat/PW-393-typescript

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,12 +2,14 @@ import assign from 'object-assign';
 
 export const merge = (a, b) => {
   const result = assign({}, a, b);
+
   for (const key in a) {
     if (!a[key] || typeof b[key] !== `object`) continue;
     assign(result, {
       [key]: assign(a[key], b[key]),
     });
   }
+
   return result;
 };
 
